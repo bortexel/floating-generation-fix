3cabfed954395d2842be17bf828bda32f125401c
RuscalWorld <me@ruscalworld.ru>
2021-05-11 22:12:02 +0300

    Initial commit

---
+++ src/main/java/ru/bortexel/floatinggenfix/Floatinggenfix.java
@@ -0,0 +1,10 @@
+package ru.bortexel.floatinggenfix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Floatinggenfix implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
